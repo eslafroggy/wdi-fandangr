060c1b9e62ca45973d2b4efe8f7ff70c946d5520
Sumeet Jain <sumeet@sumeetjain.com>
2013-07-08 10:21:18 -0700

    Rails!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fandangr::Application.config.secret_token = '1abf483694ccdc843c5f0dc0de90d3bcc37bff0959bda74d0719ac87618fffb82868b367531f350f0bfa3790734172ffe9666ae670e9fcdd24e086deba0b4ba6'
