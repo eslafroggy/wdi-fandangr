9011de6a4f723542266a0ad52d4d25518b9c9fee
Sumeet Jain <sumeet@sumeetjain.com>
2013-07-08 13:02:34 -0700

    User Factory for UserSpec

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,2 +1,6 @@
 FactoryGirl.define do
+  factory :user do
+    email "foo@bar.com"
+    password "foobar"
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,37 +1,23 @@
 require 'spec_helper'
 
 describe User do
-  it "authenticates with a valid email and password" do
-    user = User.create(
-      email: "foo@bar.com",
-      password: "foobar",
-      password_confirmation: "foobar"
-    )
+  before do
+    @user = FactoryGirl.create(:user)
+  end
 
-    expect(user.authenticate("foobar")).to eq(user)
+  it "authenticates with a valid email and password" do
+    expect(@user.authenticate("foobar")).to eq(@user)
   end
 
   it "authenticates with an incorrect password" do
-    user = User.create(
-      email: "foo@bar.com",
-      password: "foobar",
-      password_confirmation: "foobar"
-    )
-
-    expect(user.authenticate("incorrect")).to be_false
+    expect(@user.authenticate("incorrect")).to be_false
   end
 
   it "reserves a ticket to a showing" do
-    user = User.create(
-      email: "foo@bar.com",
-      password: "foobar",
-      password_confirmation: "foobar"
-    )
-
     film = Film.create(title: "The Godfather")
     showing = film.showings.create(showing_on: "2013-01-01 12:30:00")
-    ticket = user.tickets.create(showing_id: showing.id, seat: "A1")
+    ticket = @user.tickets.create(showing_id: showing.id, seat: "A1")
 
-    expect(user.tickets).to include(ticket)
+    expect(@user.tickets).to include(ticket)
   end
 end
