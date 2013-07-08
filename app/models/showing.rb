a2e4043f432713e7157d576071a5fe12ee4b2985
Sumeet Jain <sumeet@sumeetjain.com>
2013-07-08 12:52:18 -0700

    All tests pass. Woohoo!

--- app/models/showing.rb
+++ app/models/showing.rb
@@ -3,4 +3,10 @@ class Showing < ActiveRecord::Base
 
   has_many :tickets
   belongs_to :film
+
+  def available_tickets?
+    unreserved_tickets = self.tickets.where(:user_id => nil)
+
+    unreserved_tickets.length > 0
+  end
 end
