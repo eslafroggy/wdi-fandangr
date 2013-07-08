fceff120fe21944c2356b9960e6f1125e311db24
Sumeet Jain <sumeet@sumeetjain.com>
2013-07-08 15:00:58 -0700

    Use this instead - it's cleaner.

--- spec/models/showing_spec.rb
+++ spec/models/showing_spec.rb
@@ -2,15 +2,15 @@ require 'spec_helper'
 
 describe Showing do
   it "has tickets available" do
-    showing = FactoryGirl.create(:showing)
-    ticket = FactoryGirl.create(:ticket, {:showing_id => showing.id})
+    ticket = FactoryGirl.create(:ticket)
+    showing = ticket.showing
 
     expect(showing.available_tickets?).to be_true
   end
 
   it "is sold out" do
-    showing = FactoryGirl.create(:showing)
-    ticket = FactoryGirl.create(:reserved_ticket, {:showing_id => showing.id})
+    ticket = FactoryGirl.create(:reserved_ticket)
+    showing = ticket.showing
 
     expect(showing.available_tickets?).to be_false
   end
