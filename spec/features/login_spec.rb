943d201c60f1e46a9dfdf738e57aa11710cec1f5
Sumeet Jain <sumeet@sumeetjain.com>
2013-07-09 14:34:58 -0700

    Feature block

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,1 +1,15 @@
 require 'spec_helper'
+
+feature "login" do
+  scenario "with correct password" do
+
+  end
+end
+
+# SAME THING!
+#
+# describe "login" do
+#   it "with correct password" do
+#     ...
+#   end
+# end
